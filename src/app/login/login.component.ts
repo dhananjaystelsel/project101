efec3f2e20040cfee597e5e6b67176a28712b4be
dhananjaybelhekar <dhananjaystelsel@gmail.com>
2018-03-01 13:59:49 +0530

    login redirection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) {}
 
   ngOnInit() {
-  }
+    if (localStorage.getItem('isLoggedin')) {
+        this.router.navigate(['/show']);
+    }
+}
   onLoggedin() {
     localStorage.setItem('isLoggedin', 'true');
 }
