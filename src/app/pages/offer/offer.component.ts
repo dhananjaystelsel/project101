a7be52dccdeae141d3af3aee92ee72f1dded2a75
sarikadstelsel <sarikadstelsel@gmail.com>
2018-03-08 16:46:47 +0530

    add apiEndpoint

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -3,7 +3,7 @@ import { Offer} from '../../modal/offer'
 import { HttpClient, HttpResponse,HttpHeaders } from '@angular/common/http';
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
-import * as conf from '../../config';
+import { environment } from "../../../environments/environment";
 import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 
@@ -44,7 +44,7 @@ export class OfferComponent implements OnInit {
       ajax: (dataTablesParameters: any, callback) => {
         that.http
           .post<any>(
-            conf.data.api+'/dt',
+            environment.apiEndpoint+'/dt',
             dataTablesParameters, {}
           ).subscribe(resp => {
             that.persons = resp;
@@ -72,7 +72,7 @@ show(event,content){
 }
 edit(){
   console.log(this.editPersons);
- this.http.post(conf.data.api+'/editUser',this.editPersons,httpOptions)
+ this.http.post(environment.apiEndpoint+'/editUser',this.editPersons,httpOptions)
   .subscribe(result=>console.log(result) , err => console.log(err),
   () =>{ console.log('Request Completed');$('#closeModal').click()});
 }
